4b3dd2c0e239c9819b97f5e33e8468f86d5a0a55
OmerFlame <renginald.bagel@gmail.com>
2021-01-04 11:43:46 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/realm/SwiftLint.git", from: "0.39.1"),
-        .package(url: "https://github.com/OmerFlame/ADUtils.git", from: "1.0.0")
+        .package(url: "https://github.com/OmerFlame/ADUtils.git", .branch("main"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
